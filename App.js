8102e2f9b891fc643de62fabc17af7ca06349ac5
Sahil <sahil.1si18ec083@gmail.com>
2023-04-24 18:49:28 +0530

    Updating chapter 3 Laying the foundation notes

--- App.js
+++ App.js
@@ -59,5 +59,34 @@ const parent = React.createElement(
 console.log(parent);
 root.render(parent);
 
-// So we need jsx to write react so that we do not need to write nested tags using React.createElement
-// jsx is not the only way to write react we  can write it using the React.createElement and do nesting of it and render it in dom using root.render(parent)
+// So we need jsx to write react so
+// that we do not need to write nested tags using React.createElement
+// jsx is not the only way to write react
+//we  can write it using the React.createElement and do nesting of it
+// and render it in dom using root.render(parent)
+
+//JSX
+const num = 90;
+// this is a reeact element
+const jsxHeading = (
+  <h1 className="heading">
+    <p>This is my heading tag</p>
+  </h1>
+);
+
+const fun = () => {
+  return "This is my fun function";
+};
+// REACT FUNCTIONAL COMPONENT is Just a normal javascript function which returns some piece of jsx
+const Reactcomponent = () => (
+  <React.Fragment>
+    {89 + 90}
+    <p>{fun()}</p>
+    <h1>{num}</h1>
+    <p>This is my para</p>
+    <h1>{new Date().toLocaleTimeString()}</h1>
+  </React.Fragment>
+);
+setInterval(() => {
+  root.render(<Reactcomponent />);
+}, 2000);
