31c6fb6302ca42345b63c5f67b817c4278f585c4
Sahil <sahil.1si18ec083@gmail.com>
2023-04-28 19:12:23 +0530

    Added chapter 5 notes on hooks

--- Chapter 04 - Talk is Cheap, show me the code/Body.jsx
+++ Chapter 04 - Talk is Cheap, show me the code/Body.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "./Utility/applicationConfig";
+import { useState } from "react";
 const Body = () => {
   console.log(restaurantList);
+  const [arestaurantList, setRestaurantList] = useState(restaurantList);
+  const filterHandler = (event) => {
+    console.log(event);
+
+    setRestaurantList(
+      arestaurantList.filter(
+        (item, index) => Number(item.data.avgRating) >= 4.5
+      )
+    );
+  };
   return (
     <React.Fragment>
       <div className="body">
-        <div className="Search">Search</div>
+        <div className="filter">
+          <button
+            className="filter-btn"
+            onClick={(event) => filterHandler(event)}
+          >
+            Top Rated Restaurant
+          </button>
+        </div>
         <div className="res-container">
-          {restaurantList.map((item, index) => (
+          {arestaurantList.map((item, index) => (
             <RestaurantCard
               restaurantList={restaurantList}
               index={index}
